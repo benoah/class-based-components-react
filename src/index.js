f9b8134b04594ba5f73d6842dbacbc2eb7351fba
ben <noahbenaas@gmail.com>
2021-03-01 16:33:52 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+*/
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+// #1
+function App() {
+  return (
+    <div>
+      <Header />
+      <Greeting />
+    </div>
+  );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="vschool" />
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+// #2
+function Header(props) {
+  return (
+    <header>
+      <p>Welcome, {props.username}!</p>
+    </header>
+  );
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    );
+  }
+}
+
+// #3
+function Greeting() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+  return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+}
+
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
